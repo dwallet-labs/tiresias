d7b81b15b1c915fd2be9e447a099035b59dc1a5d
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-04-20 21:25:27 +0100

    Implemented ProofOfEqualityOfDiscreteLogs, but with incorrect calculation of w that makes verification fail -  awaiting input from Ofir

--- src/proofs.rs
+++ src/proofs.rs
@@ -1,4 +1,6 @@
 use core::fmt;
+use crypto_bigint::{Encoding, U4096};
+use merlin::Transcript;
 use std::error::Error;
 
 mod equality_of_discrete_logs;
@@ -15,5 +17,29 @@ impl fmt::Display for ProofError {
 impl Error for ProofError {
     fn description(&self) -> &str {
         "Verification failure!"
+    }
+}
+
+/// Defines a `TranscriptProtocol` trait for using a Merlin transcript.
+pub trait TranscriptProtocol {
+    // TODO: is field element the correct phrase here?
+    /// Append a `field_element` with the given `label`.
+    fn append(&mut self, label: &'static [u8], field_element: &U4096);
+
+    /// Compute a `label`ed challenge variable.
+    fn challenge(&mut self, label: &'static [u8]) -> U4096;
+}
+
+impl TranscriptProtocol for Transcript {
+    fn append(&mut self, label: &'static [u8], field_element: &U4096) {
+        self.append_message(label, &field_element.to_le_bytes());
+    }
+
+    // TODO: this actually needs to be a different size, kappa smth.
+    fn challenge(&mut self, label: &'static [u8]) -> U4096 {
+        let mut buf = [0u8; U4096::BYTES];
+        self.challenge_bytes(label, &mut buf);
+
+        U4096::from_le_slice(&buf)
     }
 }
--- src/proofs/equality_of_discrete_logs.rs
+++ src/proofs/equality_of_discrete_logs.rs
@@ -1,5 +1,5 @@
 use crate::paillier::EncryptionKey;
-use crate::proofs::ProofError;
+use crate::proofs::{ProofError, TranscriptProtocol};
 use crypto_bigint::consts::U4;
 use crypto_bigint::modular::runtime_mod::{DynResidue, DynResidueParams};
 use crypto_bigint::RandomMod;
@@ -21,17 +21,65 @@ impl ProofOfEqualityOfDiscreteLogs {
     pub fn prove(
         encryption_key: EncryptionKey,
         d: &U4096,
+        a: &U4096,
+        b: &U4096,
         g: &U4096,
         h: &U4096,
         rng: &mut impl CryptoRngCore,
     ) -> ProofOfEqualityOfDiscreteLogs {
-        let r = U4096::random_mod(rng, &NonZero::new(encryption_key.n).unwrap());
-        // TODO: account for the security parameter kappa
-        todo!()
+        let r = U4096::random_mod(rng, &NonZero::new(encryption_key.n).unwrap()); // TODO: account for the security parameter kappa
+        let g_hat = encryption_key.mod_n2(g).pow(&r).retrieve();
+        let h_hat = encryption_key.mod_n2(h).pow(&r).retrieve();
+
+        let mut transcript = Transcript::new(b"Proof of Equality of Discrete Logs");
+        transcript.append(b"a", &a);
+        transcript.append(b"b", &b);
+        transcript.append(b"g_hat", &g_hat);
+        transcript.append(b"h_hat", &h_hat);
+
+        let u = transcript.challenge(b"u");
+
+        // let w = r.wrapping_sub(u.mul_wide(d)); // TODO: how can this be, what's the sizes here?
+        let w = r; // TODO: use above line...
+
+        ProofOfEqualityOfDiscreteLogs {
+            a: a.clone(),
+            b: b.clone(),
+            g_hat,
+            h_hat,
+            w,
+        }
     }
 
-    pub fn verify(&self) -> Result<(), ProofError> {
-        todo!()
+    // TODO: g, h comes from the verifier right? he should know it ahead of time?
+    pub fn verify(
+        &self,
+        encryption_key: &EncryptionKey,
+        g: &U4096,
+        h: &U4096,
+    ) -> Result<(), ProofError> {
+        // TODO: need to check not zero for every member?
+
+        let mut transcript = Transcript::new(b"Proof of Equality of Discrete Logs");
+        transcript.append(b"a", &self.a);
+        transcript.append(b"b", &self.b);
+        transcript.append(b"g_hat", &self.g_hat);
+        transcript.append(b"h_hat", &self.h_hat);
+
+        let u = transcript.challenge(b"u");
+
+        // TODO: in the paper, no mod is specified for the powers, is N^2 the correct mod?
+        if (encryption_key.mod_n2(&g).pow(&self.w) * encryption_key.mod_n2(&self.a).pow(&u))
+            .retrieve()
+            == self.g_hat
+            && (encryption_key.mod_n2(h).pow(&self.w) * encryption_key.mod_n2(&self.b).pow(&u))
+                .retrieve()
+                == self.h_hat
+        {
+            Ok(())
+        } else {
+            Err(ProofError {})
+        }
     }
 }
 
@@ -46,17 +94,30 @@ mod tests {
     #[test]
     fn valid_proof_verifies() {
         let encryption_key = EncryptionKey::new(N);
+        let a = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
+        let b = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
         let g = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
         let h = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
 
-        let proof = ProofOfEqualityOfDiscreteLogs::prove(encryption_key, &D, &g, &h, &mut OsRng);
+        let proof = ProofOfEqualityOfDiscreteLogs::prove(
+            encryption_key.clone(),
+            &D,
+            &a,
+            &b,
+            &g,
+            &h,
+            &mut OsRng,
+        );
 
-        assert!(proof.verify().is_ok());
+        assert!(proof.verify(&encryption_key, &g, &h).is_ok());
     }
 
     #[test]
     fn invalid_proof_fails_verification() {
         let encryption_key = EncryptionKey::new(N);
+
+        let g = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
+        let h = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
 
         /* First generate a truly random proof and make sure it fails */
         let invalid_proof = ProofOfEqualityOfDiscreteLogs {
@@ -67,36 +128,45 @@ mod tests {
             w: U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap()),
         };
 
-        assert!(invalid_proof.verify().is_err());
+        assert!(invalid_proof.verify(&encryption_key, &g, &h).is_err());
 
         /* Now generate a valid proof, and make sure that if we change any field it fails */
+        let a = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
+        let b = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
         let g = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
         let h = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
 
-        let valid_proof =
-            ProofOfEqualityOfDiscreteLogs::prove(encryption_key.clone(), &D, &g, &h, &mut OsRng);
+        let valid_proof = ProofOfEqualityOfDiscreteLogs::prove(
+            encryption_key.clone(),
+            &D,
+            &a,
+            &b,
+            &g,
+            &h,
+            &mut OsRng,
+        );
 
         let mut invalid_proof = valid_proof.clone();
 
         invalid_proof.a = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
-        assert!(invalid_proof.verify().is_err());
+        assert!(invalid_proof.verify(&encryption_key, &g, &h).is_err());
 
         invalid_proof = valid_proof.clone();
         invalid_proof.b = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
-        assert!(invalid_proof.verify().is_err());
+        assert!(invalid_proof.verify(&encryption_key, &g, &h).is_err());
 
         invalid_proof = valid_proof.clone();
         invalid_proof.g_hat =
             U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
-        assert!(invalid_proof.verify().is_err());
+        assert!(invalid_proof.verify(&encryption_key, &g, &h).is_err());
 
         invalid_proof = valid_proof.clone();
         invalid_proof.h_hat =
             U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
-        assert!(invalid_proof.verify().is_err());
+        assert!(invalid_proof.verify(&encryption_key, &g, &h).is_err());
 
-        invalid_proof = valid_proof.clone();
+        invalid_proof = valid_proof;
         invalid_proof.w = U4096::random_mod(&mut OsRng, &NonZero::new(encryption_key.n).unwrap());
-        assert!(invalid_proof.verify().is_err());
+        assert!(invalid_proof.verify(&encryption_key, &g, &h).is_err());
     }
 }
