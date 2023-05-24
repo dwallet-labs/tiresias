13bb50654b877f8c20814a1a6f309b99853e1ee3
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-05-24 14:50:38 +0300

    Returned to the proof format from the article. There's still a check that I believe I am missing in verify().

--- src/proofs/equality_of_discrete_logs.rs
+++ src/proofs/equality_of_discrete_logs.rs
@@ -14,14 +14,13 @@ use crate::{
     ProofOfEqualityOfDiscreteLogsRandomnessSizedNumber,
 };
 
-/// A proof of equality of discrete logs
-/// This is an optimized form of the proof which does not include the base (squared) & ciphertext
-/// (squared) randomizers. Instead, it includes the challenge, from which the verifier can recover
-/// the randomizers and verify the challenge.
+/// A proof of equality of the discrete logs of `self.base_squared_randomizer` and `self.ciphertext_squared_randomizer`
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub(crate) struct ProofOfEqualityOfDiscreteLogs {
-    // The challenge $e \in \mathbb{Z}$.
-    challenge: ComputationalSecuritySizedNumber,
+    // The base (squared) randomizer $\hat{g} \in \mathbb{Z}_{N^2}^*$.
+    base_squared_randomizer: PaillierModulusSizedNumber,
+    // The ciphertext (biquadrated) randomizer $\hat{h} \in \mathbb{Z}_{N^2}^*$.
+    ciphertext_biquadrated_randomizer: PaillierModulusSizedNumber,
     // The response $z \in \mathbb{Z}$.
     response: ProofOfEqualityOfDiscreteLogsRandomnessSizedNumber,
 }
@@ -105,7 +104,8 @@ impl ProofOfEqualityOfDiscreteLogs {
         let response = randomizer.wrapping_sub(&((challenge * secret_key_share).into()));
 
         ProofOfEqualityOfDiscreteLogs {
-            challenge,
+            base_squared_randomizer,
+            ciphertext_biquadrated_randomizer,
             response,
         }
     }
@@ -169,6 +169,7 @@ impl ProofOfEqualityOfDiscreteLogs {
         // Note that if we'd have perform this check prior to squaring, it wouldn't have suffice;
         // take e.g. g = N != 0 -> g^2 = N^2 mod N^2 = 0 (accepting this value would have allowed
         // bypassing of the proof).
+        // TODO: correct this (how to check self.ciphertext_biquadrated_randomizer and self.base_squared_randomizer)
         if base_squared == PaillierModulusSizedNumber::ZERO
             || ciphertext_biquadrated == PaillierModulusSizedNumber::ZERO
             || public_verification_key_squared == PaillierModulusSizedNumber::ZERO
@@ -189,36 +190,35 @@ impl ProofOfEqualityOfDiscreteLogs {
                 &self.response,
             );
 
+        transcript.append_statement(b"base (squared) randomizer", &self.base_squared_randomizer);
+        transcript.append_statement(
+            b"ciphertext (biquadrated) randomizer",
+            &self.ciphertext_biquadrated_randomizer,
+        );
+
+        let challenge: ComputationalSecuritySizedNumber = transcript.challenge(b"challenge");
+
         let public_verification_key_squared_raised_to_the_challenge =
             <PaillierRingElement as Pow<ComputationalSecuritySizedNumber>>::pow(
                 &public_verification_key_squared.as_ring_element(n2),
-                &self.challenge,
+                &challenge,
             );
 
         let decryption_share_squared_raised_to_the_challenge =
             <PaillierRingElement as Pow<ComputationalSecuritySizedNumber>>::pow(
                 &decryption_share_squared.as_ring_element(&n2),
-                &self.challenge,
+                &challenge,
             );
 
-        // The base randomizer $\hat{g} \in \mathbb{Z}_{N^2}^*$.
-        let base_squared_randomizer = (base_squared_raised_to_the_response
+        if (base_squared_raised_to_the_response
             * public_verification_key_squared_raised_to_the_challenge)
-            .as_natural_number();
-
-        // The ciphertext randomizer $\hat{h} \in \mathbb{Z}_{N^2}^*$.
-        let ciphertext_biquadrated_randomizer = (ciphertext_biquadrated_raised_to_the_response
-            * decryption_share_squared_raised_to_the_challenge)
-            .as_natural_number();
-
-        transcript.append_statement(b"base (squared) randomizer", &base_squared_randomizer);
-        transcript.append_statement(
-            b"ciphertext (biquadrated) randomizer",
-            &ciphertext_biquadrated_randomizer,
-        );
-
-        let challenge: ComputationalSecuritySizedNumber = transcript.challenge(b"challenge");
-        if challenge == self.challenge {
+            .as_natural_number()
+            == self.base_squared_randomizer
+            && (ciphertext_biquadrated_raised_to_the_response
+                * decryption_share_squared_raised_to_the_challenge)
+                .as_natural_number()
+                == self.ciphertext_biquadrated_randomizer
+        {
             return Ok(());
         }
         Err(ProofError {})
@@ -608,59 +608,6 @@ mod tests {
             .is_ok());
     }
 
-    fn craft_proof(
-        n2: &PaillierModulusSizedNumber,
-        base: &PaillierModulusSizedNumber,
-        ciphertext_squared: &PaillierModulusSizedNumber,
-        public_verification_key: &PaillierModulusSizedNumber,
-        decryption_share: &PaillierModulusSizedNumber,
-    ) -> ProofOfEqualityOfDiscreteLogs {
-        let base_squared = base
-            .as_ring_element(n2)
-            .pow_bounded_exp(&PaillierModulusSizedNumber::from(2u8), 2);
-        let ciphertext_biquadrated = ciphertext_squared
-            .as_ring_element(n2)
-            .pow_bounded_exp(&PaillierModulusSizedNumber::from(2u8), 2);
-        let public_verification_key_squared = public_verification_key
-            .as_ring_element(n2)
-            .pow_bounded_exp(&PaillierModulusSizedNumber::from(2u8), 2);
-        let decryption_share_squared = decryption_share
-            .as_ring_element(n2)
-            .pow_bounded_exp(&PaillierModulusSizedNumber::from(2u8), 2);
-
-        let mut transcript = Transcript::new(b"Proof of Equality of Discrete Logs");
-        transcript.append_statement(b"N (squared)", n2);
-        transcript.append_statement(b"base (squared)", &base_squared.as_natural_number());
-        transcript.append_statement(
-            b"ciphertext (biquadrated)",
-            &ciphertext_biquadrated.as_natural_number(),
-        );
-        transcript.append_statement(
-            b"public verification key (squared)",
-            &public_verification_key_squared.as_natural_number(),
-        );
-        transcript.append_statement(
-            b"decryption share (squared)",
-            &decryption_share_squared.as_natural_number(),
-        );
-        transcript.append_statement(
-            b"base (squared) randomizer",
-            &PaillierModulusSizedNumber::ZERO,
-        );
-        transcript.append_statement(
-            b"ciphertext (biquadrated) randomizer",
-            &PaillierModulusSizedNumber::ZERO,
-        );
-
-        let challenge: ComputationalSecuritySizedNumber = transcript.challenge(b"challenge");
-        let wrong_response = ProofOfEqualityOfDiscreteLogsRandomnessSizedNumber::from_be_hex("D09B42E9E0E7923F33FB1A81A72545DBF6710C72D43F1CDA195A2461A414BE228F4DA35BF1056AC4E58D04167D66C0E69417ED428CE39CE810BFA0AA589917E22C2D9519EC475BDCBDE46B5898E730F3B15D58E5906848106909B5A423F3DCA73F7C130424442E171CDFD7F58A808D974766A576D90C72119084A91156989E8DBB100329B81F1EC8E592CF84802FACC1049ED522C83D13E52CC45630DD70A2045F657E209496B4218CD6B394BBDC799CABB393BF8A3AEC3379BF40F19FA40C5EC4A375C6946A56778F3CAFA43D319F1116E36D88D447A89EF2D7BD822E15FB09F1C4E3B00FEB167C715DF1318DC0D1C3ACE3B739C0E1C886E43AB49EFFD1E697CE0F8F68BA31468DED59B466F0249A9AE395A1FC4533164AD7F543D1195C11F99B9D18E08464A10CB6CC42FD39E838B0CD5512435F1CC1A29D4B60A013700D5D5E95459C354A140533B7FA938A9E7E6E7ECFB233BC80A40E495577F901DE5229B73823473EACCFC058320F98CA2D124E47E5412BA8C88517077DE964830A487E80C8A4A49343B7000AE2D702998F8CB3A812BB01971BC47BA514A5723382AFD3AF3EC5F9D9C17FC33157C49B8E84453B80F636C92600D31A4EEF6E92694B99C02489443351319CC576B763E4C293A0CDDF620A38DB381D252D8FDD2DF239B67404586DA8C34A4A993351AAC1188DF6D26CDAEF1B3A46599C3A310376C7FFB2CA75AE4A32E539D34615269A9D2BE93C4CF8B557677E04737AA0395E392AE6A804");
-
-        ProofOfEqualityOfDiscreteLogs {
-            challenge,
-            response: wrong_response,
-        }
-    }
-
     #[test]
     fn invalid_proof_fails_verification() {
         let n2 = N.square();
@@ -676,11 +623,12 @@ mod tests {
         let wrong_public_verification_key = PaillierModulusSizedNumber::from_be_hex("891875311F6A7F18F7347C96A61922B21E5CA4F042A3BF5E7F46EA43AF927CB1806417A800CE327D45EC6846F3FBCF898F5BF8DE76A8A84B762E44043ADC1E2BED2C8BF7C017ADE77342DA758933360063BE7272C22467D98B99578BACBAE7D0B332CE246940F577B8A328F0DC2007A6E132C8B138A669940E81A499B10D5396658F9E8E6B4D01AB5E7A2B7401C11615628F53086DE498D4501B07C4F35D096E04608E129F09BC90DA051DE836FA143C48DCB968135C85784D02340D6EE45A8345127C6CC8A2C5AF837D64005307A64844A8198DCD0FA493DFB717AEB9022FA89B32F4643EF2F2C963586372241768D050B2AFE3A9092394E1AD49DFDB3E013D318E4D9162747F41CD4F4DBBA67642AD57563FA6A1203F2839B30D27F2D39AF50A70BA8337FA260A1AF6763D633F9CCF60F27C3D01A884F623A31977ADC62DDC2586CCF9C395C8DF3E513F92E377E9D11673BA1DB247D514CE8CBBC0BF2426167459914437077A020B710B22FE44BBC794FE4166175C5754137F0CE9B9B6DB8C622C4437D162E4731D3939E35413416710BB23B2A59FAED88765523E38ABB4134649C87A05935F1CAD26C6F3C61562EABF11ED607D4B7EB5B9A5C36405BAF548F88561B47625099BFE46B73CD2E4D6EF62A1A2A843297B8CAB546E46461C1293FC292C9C765CA3403C1C034B71973693E93C2DC3B4D8AFC872F6456B746742FF");
         let wrong_decryption_share = PaillierModulusSizedNumber::from_be_hex("058884DF955E54100E0E5F22DB059C993EE98BA75738B0F1A4383F9B38E5E79585F3290B04687C318CA471AF303E193BB303F1A659AD60204E3BF811F222BA4D14C92F3FC4B957E9718944E631373B9BA0E20F53F2260219B03F00D2691DA1E928489DDC9FC45F198FD162C8DBAC30653F4DEB3B00CFB58F534E93941B045CD54D4879BED79CD0E553D6DE0688E4FB7EBA375CD63FDE2E205387A4D30D7B0ED552D03E44AA17BB152BD8A05B449A15AB6DCB06BC912CE4691D2D2F0604A8B2218668416183F99923F9FB1BA3EFF1CE6D1CA3390DC062157CE7002AE6D5C3A580BA076F36308182C40B1E8C81140DDDA0E99FDC54C2A8330620A7C8048705E000AF78B3FA3EBF892157BC4CEB934B8E5822EAC596FC00E2D28F4B5372E80E5CF722D17035ABA8FF642C6ADE11D39E3E9DD9B034B5256E671B8B0C291D042C70BF2896E1ACD6BED1F1055EE01C368FC70C896A20479534C2A7300603524B7A6BA0206404AB289D5752BDD57C56B72CD47060224D9B43B2F8AC3D91AC605814A1FBB44C17B5283D0BDC56658B1D9823A74048CFE0A5001A80EC1F8764A96305C65C5B66F52C9A2D8C9C4F9247907716C6E18BA5F6747A59F25FA3F6A10BDCC5369481A3DB861FA1A95E3F2A5A6C054807E0386AF7FF8C6D3DFC81509FDC55E749E8C9EAB44D46C6A1E75AD364F0C178ACC62875BF626D9354283968AFF958FAD855");
         let wrong_response = ProofOfEqualityOfDiscreteLogsRandomnessSizedNumber::from_be_hex("D09B42E9E0E7923F33FB1A81A72545DBF6710C72D43F1CDA195A2461A414BE228F4DA35BF1056AC4E58D04167D66C0E69417ED428CE39CE810BFA0AA589917E22C2D9519EC475BDCBDE46B5898E730F3B15D58E5906848106909B5A423F3DCA73F7C130424442E171CDFD7F58A808D974766A576D90C72119084A91156989E8DBB100329B81F1EC8E592CF84802FACC1049ED522C83D13E52CC45630DD70A2045F657E209496B4218CD6B394BBDC799CABB393BF8A3AEC3379BF40F19FA40C5EC4A375C6946A56778F3CAFA43D319F1116E36D88D447A89EF2D7BD822E15FB09F1C4E3B00FEB167C715DF1318DC0D1C3ACE3B739C0E1C886E43AB49EFFD1E697CE0F8F68BA31468DED59B466F0249A9AE395A1FC4533164AD7F543D1195C11F99B9D18E08464A10CB6CC42FD39E838B0CD5512435F1CC1A29D4B60A013700D5D5E95459C354A140533B7FA938A9E7E6E7ECFB233BC80A40E495577F901DE5229B73823473EACCFC058320F98CA2D124E47E5412BA8C88517077DE964830A487E80C8A4A49343B7000AE2D702998F8CB3A812BB01971BC47BA514A5723382AFD3AF3EC5F9D9C17FC33157C49B8E84453B80F636C92600D31A4EEF6E92694B99C02489443351319CC576B763E4C293A0CDDF620A38DB381D252D8FDD2DF239B67404586DA8C34A4A993351AAC1188DF6D26CDAEF1B3A46599C3A310376C7FFB2CA75AE4A32E539D34615269A9D2BE93C4CF8B557677E04737AA0395E392AE6A804");
-        let wrong_challenge =
-            ComputationalSecuritySizedNumber::from_be_hex("5C39491D60D4D0CC8914C67D6CF41212");
+        let wrong_base_squared_randomizer = PaillierModulusSizedNumber::from_be_hex("1B8DC8C817CAF6ABF6B3BE337E6D723BABFC968213DFC5BE5E9B524B380A58BE0ADB12576177AFC3604D6DC28303D5F5A0B54303B033AA73C10DA59008C6B5806CE3C781096BBB32CBC3E5FF70B62A1D6F0525152E70F8FA5751249EF7B3E21DBB6DD735C11CA6282863B4FDA8FE2C993F03C3D5E008F28218FA57647F0526BCF355CB2D589DA8D2CEED3E2013B55A803300544368AF573184CC7472C9B33CB39B541E2D388384BA86E913A62B6C61D65755FD75CD79B7ACBF41EF8755E8C50C1E9D5D01B0FFCAEA990194EA41FC4591032C79517B10F2DBB23868300B222BE6DFA7B974A7BCAC395CAE72D655F2EDBFF6D6407DF874600CE7688EBFE440336681EA1A44395F67D1CC9D1C092B889D3049465D0FC21C74FA3642018A73C9510FDF6C89CE0AAC3DCC089A3092BC029F518548B2D158FBBDCEA4634D0EEDBA32E2A095886F995F5C3D6C146371F633145823DA4AAA022A62CF7F9D76597F6A550F41FEA5EC7310ED59E1134F5F86B84AFAD4911270361370B3313CD46F01CEDDCAEA1A580F5A4D3C58888F5802BEC2FED81AC7733D3399A5E6D9D3EF8B136906694709E0C0348EF083A0106AC41979289A41B0356362A85BA40A6AEA0191A83E5C48CE580FAA9AE1DE8462E8CEBDCB3BC879C5F5AFB4F2B136E057BFB314AB19CC64018F933647FA18BD4CB6F75BE614708EF748EDBA2A1BEDB115E8BBEB2B96CC");
+        let wrong_ciphertext_biquadrated_randomizer = PaillierModulusSizedNumber::from_be_hex("2EAA0D625BD67781CFFCC4563365C37CDCEC8782B451703D4130F7B05E4D080EFF1668E8E96125EE991C45076AB92EFA40232A2C150A96A8DB72F9FB1E7EC57924A13465113ECF9CA575C312C11841C83935B81137B3172C5AB86CCF7EE400D525F5FDA24194F6BCEB4ECDA99EDCE509DDA1B5106EECE1962FFA65FE6B359E5524685ECB5C785B8B0F91744BB2B50EBEE981099AC4B66F6CCF01B1D16F6611D0846EAA44E20C4895CC77CC4CECD1A2AA34CCC97AE0D91C9438FBD0A59A66AB669BD35D78869DD9801C0B13CBBF202F8E21C21B4B76C006DE01A93DC708DE5777B1F305598DEC6552F92DB4166D10B784C6E897579265CA6C48721947F5CF0FC2B65C1EF15A0A62AF54478DD2E3275942A6D2E628C7FD56F1522DD6B251ECD129B93CDC76167A16B52711AB02315EA7D53C0F5F15403AC7BDC1DB3D65D714DA38ED350590D4ACB2B8CC6F4597C4CBCE311F8EA71B3B5783A3C57BACAA7D065EA12190DD982ADC78ECBE0F864016EDB59E9097D6D0DBECAA5F9E8272F1204246059AE42348DC978AF8F0E82BC13940559A7900AE10F343253F97611493EFA673FF74C695476A90FAD5AE734885C86C895F1CFF3E4731A3569F2295B119A46D48632BEB2576D0C0435E49C61FDFA0960C31E79D6BEFF0A1676F8F15A45B3E74B1905035DC3414B812B4253896CB04EDD6C9C4B9822FC2A2A567DEDB3F8730BBD2BD");
 
         let invalid_proof = ProofOfEqualityOfDiscreteLogs {
-            challenge: wrong_challenge,
+            base_squared_randomizer: wrong_base_squared_randomizer,
+            ciphertext_biquadrated_randomizer: wrong_ciphertext_biquadrated_randomizer,
             response: wrong_response,
         };
 
@@ -705,13 +653,11 @@ mod tests {
             .as_natural_number();
 
         // Try to fool verification with zeroed out fields
-        let crafted_proof = craft_proof(
-            &n2,
-            &BASE,
-            &ciphertext_squared,
-            &PaillierModulusSizedNumber::ZERO,
-            &PaillierModulusSizedNumber::ZERO,
-        );
+        let crafted_proof = ProofOfEqualityOfDiscreteLogs {
+            base_squared_randomizer: PaillierModulusSizedNumber::ZERO,
+            ciphertext_biquadrated_randomizer: PaillierModulusSizedNumber::ZERO,
+            response: wrong_response,
+        };
 
         assert!(crafted_proof
             .verify(
@@ -724,10 +670,20 @@ mod tests {
             .is_err());
 
         // Try to fool verification with fields that their square is zero mod N^2 (e.g. N)
-        let crafted_proof = craft_proof(&n2, &BASE, &ciphertext_squared, &N.into(), &N.into());
+        let crafted_proof = ProofOfEqualityOfDiscreteLogs {
+            base_squared_randomizer: (N * LargeBiPrimeSizedNumber::from(2u8)),
+            ciphertext_biquadrated_randomizer: (N * LargeBiPrimeSizedNumber::from(2u8)),
+            response: wrong_response,
+        };
 
         assert!(crafted_proof
-            .verify(&N, &BASE, &ciphertext_squared, &N.into(), &N.into(),)
+            .verify(
+                &N,
+                &BASE,
+                &ciphertext_squared,
+                &(N * LargeBiPrimeSizedNumber::from(2u8)),
+                &(N * LargeBiPrimeSizedNumber::from(2u8)),
+            )
             .is_err());
 
         // Now generate a valid proof, and make sure that if we change any field it fails
@@ -783,7 +739,19 @@ mod tests {
             .is_err());
 
         let mut invalid_proof = valid_proof.clone();
-        invalid_proof.challenge = wrong_challenge;
+        invalid_proof.base_squared_randomizer = wrong_base_squared_randomizer;
+        assert!(invalid_proof
+            .verify(
+                &N,
+                &BASE,
+                &ciphertext_squared,
+                &public_verification_key,
+                &decryption_share,
+            )
+            .is_err());
+
+        let mut invalid_proof = valid_proof.clone();
+        invalid_proof.ciphertext_biquadrated_randomizer = wrong_ciphertext_biquadrated_randomizer;
         assert!(invalid_proof
             .verify(
                 &N,
@@ -810,7 +778,7 @@ mod tests {
 
 #[cfg(feature = "benchmarking")]
 mod benches {
-    use criterion::{BatchSize, Criterion};
+    use criterion::Criterion;
     use crypto_bigint::{NonZero, RandomMod};
     use rand_core::OsRng;
     use std::iter;
