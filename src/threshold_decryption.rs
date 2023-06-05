2f48ccafbd1eb85a3369ca9d36d4df98d51c6f8c
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-06-05 20:31:20 +0300

    Deriving `Clone` for `Message`

--- src/threshold_decryption.rs
+++ src/threshold_decryption.rs
@@ -6,6 +6,7 @@ use crate::{PaillierModulusSizedNumber, ProofOfEqualityOfDiscreteLogs};
 mod decryption_key_share;
 mod precomputed_values;
 
+#[derive(Clone)]
 pub struct Message {
     decryption_shares: Vec<PaillierModulusSizedNumber>,
     proof: ProofOfEqualityOfDiscreteLogs,
