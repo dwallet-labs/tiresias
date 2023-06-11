9ab7d7c04f8a0ada5b7db549abf00d2aa0ca4dc0
Yehonatan Cohen Scaly <scalys7@gmail.com>
2023-06-11 20:11:33 +0300

    Computing via formula

--- src/decryption_key_share.rs
+++ src/decryption_key_share.rs
@@ -15,6 +15,7 @@ use crate::{
     proofs::ProofOfEqualityOfDiscreteLogs,
     AsNaturalNumber, AsRingElement, EncryptionKey, Error, LargeBiPrimeSizedNumber, Message,
     PaillierModulusSizedNumber, PaillierRingElement, Result, SecretKeyShareSizedNumber,
+    MAX_PLAYERS,
 };
 
 #[derive(Clone)]
@@ -43,7 +44,8 @@ impl DecryptionKeyShare {
         decryption_key_share: SecretKeyShareSizedNumber,
         precomputed_values: PrecomputedValues,
     ) -> DecryptionKeyShare {
-        // TODO: assert n < 1000
+        assert!(usize::from(n) <= MAX_PLAYERS);
+
         let base = precomputed_values
             .n_factorial
             .iter()
@@ -417,7 +419,7 @@ mod tests {
     use super::*;
     use crate::{
         secret_sharing::shamir::Polynomial,
-        tests::{BASE, CIPHERTEXT, N, N2, P, Q, SECRET_KEY},
+        tests::{BASE, CIPHERTEXT, N, N2, SECRET_KEY},
         LargeBiPrimeSizedNumber, LargePrimeSizedNumber,
         SecretSharingPolynomialCoefficientSizedNumber,
     };
@@ -557,10 +559,6 @@ mod tests {
 
         // Do a "trusted dealer" setup, in real life we'd have the secret shares as an output of the
         // DKG.
-        let phi = (P.wrapping_sub(&LargePrimeSizedNumber::ONE))
-            * (Q.wrapping_sub(&LargePrimeSizedNumber::ONE));
-        let n_phi_n = phi * N;
-
         let mut coefficients: Vec<Wrapping<SecretKeyShareSizedNumber>> = iter::repeat_with(|| {
             Wrapping(SecretKeyShareSizedNumber::from(
                 SecretSharingPolynomialCoefficientSizedNumber::random(&mut OsRng),
@@ -679,8 +677,6 @@ mod benches {
         g.sample_size(10);
 
         let n = LargeBiPrimeSizedNumber::from_be_hex("97431848911c007fa3a15b718ae97da192e68a4928c0259f2d19ab58ed01f1aa930e6aeb81f0d4429ac2f037def9508b91b45875c11668cea5dc3d4941abd8fbb2d6c8750e88a69727f982e633051f60252ad96ba2e9c9204f4c766c1c97bc096bb526e4b7621ec18766738010375829657c77a23faf50e3a31cb471f72c7abecdec61bdf45b2c73c666aa3729add2d01d7d96172353380c10011e1db3c47199b72da6ae769690c883e9799563d6605e0670a911a57ab5efc69a8c5611f158f1ae6e0b1b6434bafc21238921dc0b98a294195e4e88c173c8dab6334b207636774daad6f35138b9802c1784f334a82cbff480bb78976b22bb0fb41e78fdcb8095");
-        let p = LargePrimeSizedNumber::from_be_hex("f1b165ca594631cd4dfe1f360eec12e934f8c0c36199e39e53e427b674160f062f8ca237cd0ff687b04c576d6803c3998ee799633521c56baaa74eaf5a44b9895974e221d2fb54b400a8fe0597ffec381b1ebadf5f851af0edf7f49725e899ddd1fd42864101b82878a97a9aa0c193b3b30750989953a7743597eee210452001");
-        let q = LargePrimeSizedNumber::from_be_hex("a037500622d17a20eaf5aa15486d878cef4e76cb52d98b872f03e834804352e315404a2916aa5bb7b62c4650d97c8b163e9f74f72fbf49e54322280c17fc7aefa9eee8328586d91e957e2e79965009e9298337058e3771129a052bcd23eac78217e982d1b1768d06ef424ea68a417856d309499954721a1c64049dec298fe095");
         let n2 = n.square();
         let secret_key = PaillierModulusSizedNumber::from_be_hex("19d698592b9ccb2890fb84be46cd2b18c360153b740aeccb606cf4168ee2de399f05273182bf468978508a5f4869cb867b340e144838dfaf4ca9bfd38cd55dc2837688aed2dbd76d95091640c47b2037d3d0ca854ffb4c84970b86f905cef24e876ddc8ab9e04f2a5f171b9c7146776c469f0d90908aa436b710cf4489afc73cd3ee38bb81e80a22d5d9228b843f435c48c5eb40088623a14a12b44e2721b56625da5d56d257bb27662c6975630d51e8f5b930d05fc5ba461a0e158cbda0f3266408c9bf60ff617e39ae49e707cbb40958adc512f3b4b69a5c3dc8b6d34cf45bc9597840057438598623fb65254869a165a6030ec6bec12fd59e192b3c1eefd33ef5d9336e0666aa8f36c6bd2749f86ea82290488ee31bf7498c2c77a8900bae00efcff418b62d41eb93502a245236b89c241ad6272724858122a2ebe1ae7ec4684b29048ba25b3a516c281a93043d58844cf3fa0c6f1f73db5db7ecba179652349dea8df5454e0205e910e0206736051ac4b7c707c3013e190423532e907af2e85e5bb6f6f0b9b58257ca1ec8b0318dd197f30352a96472a5307333f0e6b83f4f775fb302c1e10f21e1fcbfff17e3a4aa8bb6f553d9c6ebc2c884ae9b140dd66f21afc8610418e9f0ba2d14ecfa51ff08744a3470ebe4bb21bd6d65b58ac154630b8331ea620673ffbabb179a971a6577c407a076654a629c7733836c250000");
         let base: PaillierModulusSizedNumber = PaillierModulusSizedNumber::from_be_hex("03B4EFB895D3A85104F1F93744F9DB8924911747DE87ACEC55F1BF37C4531FD7F0A5B498A943473FFA65B89A04FAC2BBDF76FF14D81EB0A0DAD7414CF697E554A93C8495658A329A1907339F9438C1048A6E14476F9569A14BD092BCB2730DCE627566808FD686008F46A47964732DC7DCD2E6ECCE83F7BCCAB2AFDF37144ED153A118B683FF6A3C6971B08DE53DA5D2FEEF83294C21998FC0D1E219A100B6F57F2A2458EA9ABCFA8C5D4DF14B286B71BF5D7AD4FFEEEF069B64E0FC4F1AB684D6B2F20EAA235892F360AA2ECBF361357405D77E5023DF7BEDC12F10F6C35F3BE1163BC37B6C97D62616260A2862F659EB1811B1DDA727847E810D0C2FA120B18E99C9008AA4625CF1862460F8AB3A41E3FDB552187E0408E60885391A52EE2A89DD2471ECBA0AD922DEA0B08474F0BED312993ECB90C90C0F44EF267124A6217BC372D36F8231EB76B0D31DDEB183283A46FAAB74052A01F246D1C638BC00A47D25978D7DF9513A99744D8B65F2B32E4D945B0BA3B7E7A797604173F218D116A1457D20A855A52BBD8AC15679692C5F6AC4A8AF425370EF1D4184322F317203BE9678F92BFD25C7E6820D70EE08809424720249B4C58B81918DA02CFD2CAB3C42A02B43546E64430F529663FCEFA51E87E63F0813DA52F3473506E9E98DCD3142D830F1C1CDF6970726C190EAE1B5D5A26BC30857B4DF639797895E5D61A5EE");
@@ -729,9 +725,6 @@ mod benches {
 
         // Do a "trusted dealer" setup, in real life we'd have the secret shares as an output of the
         // DKG.
-        let phi = (p.wrapping_sub(&LargePrimeSizedNumber::ONE))
-            * (q.wrapping_sub(&LargePrimeSizedNumber::ONE));
-        let n_phi_n: PaillierModulusSizedNumber = phi * n;
 
         for (threshold, num_parties) in [(6, 10), (67, 100), (667, 1000)] {
             let mut coefficients: Vec<Wrapping<SecretKeyShareSizedNumber>> =
